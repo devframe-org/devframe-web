68eb8be76a6df2f0666b134e62d37b8e7e4227de
kcd <kcd@14.32.152.212>
2018-10-08 22:52:32 +0900

    1

--- src/main/java/org/devframe/web/home/controller/HomeController.java
+++ src/main/java/org/devframe/web/home/controller/HomeController.java
@@ -4,11 +4,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Controller
 public class HomeController {
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String index() {
+		log.debug("index");
+
 		return "home/homeIndex";
 	}
 
--- src/main/java/org/devframe/web/sample01/vo/Sample01VO.java
+++ src/main/java/org/devframe/web/sample01/vo/Sample01VO.java
@@ -1,5 +1,8 @@
 package org.devframe.web.sample01.vo;
 
+import lombok.Data;
+
+@Data
 public class Sample01VO {
 
 }
